f9e23c821d7e3b8135ba0201d130f1027d211062
Vaibhav T <vtalekar233@gmail.com>
2025-10-03 11:17:31 +0530

    Updated code with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Header from './components/Header/Header'
-import Main from './pages/Main'
-import ProductDetails from './pages/ProductDetails'
-import Footer from './components/Footer/Footer'
-
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Main from './pages/Main';
+import ProductDetails from './pages/ProductDetails';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <Routes>
           <Route path='/' element={<Main />} />
-          <Route path='/product/:productId' element={<ProductDetails/>} />
+          <Route path='/product/:productId' element={<ProductDetails />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
